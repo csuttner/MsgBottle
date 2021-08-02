cc68622be40efdf52323f6754ccd1c6eb576d445
Clay Suttner <csuttner@gmail.com>
2021-08-02 15:48:14 -0700

    Adds base project

---
+++ MsgBottle/MsgBottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MsgBottleApp.swift
+//  MsgBottle
+//
+//  Created by Clay Suttner on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MsgBottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
